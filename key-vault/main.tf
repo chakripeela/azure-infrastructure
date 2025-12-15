a7ce238e6b1aab8f7e9fd04b6fae9adf9488ffdf
SaiJitendraKalli <kallisaijitendra2000@gmail.com>
2025-12-16 00:07:31 +0530

    Update Key Vault name format for consistency with application naming conventions

--- key-vault/main.tf
+++ key-vault/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_key_vault" "key_vault" {
-  name                       = "kv-data-maes-${var.location}"
+  name                       = "kv-${var.application_name}-${var.location}"
   location                   = var.location
   resource_group_name        = var.resource_group_name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
