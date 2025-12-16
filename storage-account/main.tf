3277005c359d0935fdab0e524a4c96cdf8048f53
SaiJitendraKalli <kallisaijitendra2000@gmail.com>
2025-12-16 22:53:26 +0530

    Update workflow run names for clarity; modify storage container resource to use toset for improved handling

--- storage-account/main.tf
+++ storage-account/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_storage_account" "storage_account" {
 }
 
 resource "azurerm_storage_container" "containers" {
-  for_each              = var.containers
+  for_each              = toset(var.containers)
   name                  = each.value
   storage_account_id    = azurerm_storage_account.storage_account.id
   container_access_type = "private"
