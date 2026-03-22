19f08312fdb67f7cc4b127b63781045be63d0e49
Chakravarthy Peela <chakripeela@gmail.com>
2026-03-22 12:29:47 +0530

    fix issues with keyvault

--- keyvault.tf
+++ keyvault.tf
@@ -9,30 +9,41 @@ resource "azurerm_key_vault" "api_secrets" {
 
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
+}
 
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+resource "azurerm_key_vault_access_policy" "current" {
+  key_vault_id = azurerm_key_vault.api_secrets.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
 
-    secret_permissions = [
-      "Get",
-      "List",
-      "Set",
-      "Delete",
-      "Recover",
-      "Purge"
-    ]
-  }
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete",
+    "Recover",
+    "Purge"
+  ]
+}
+
+resource "time_sleep" "wait_for_key_vault_policy" {
+  depends_on = [azurerm_key_vault_access_policy.current]
+
+  create_duration = "30s"
 }
 
 resource "azurerm_key_vault_secret" "db_server" {
   name         = "db-server"
   value        = module.sql.sql_server_fqdn
   key_vault_id = azurerm_key_vault.api_secrets.id
+
+  depends_on = [time_sleep.wait_for_key_vault_policy]
 }
 
 resource "azurerm_key_vault_secret" "db_name" {
   name         = "db-name"
   value        = module.sql.sql_database_name
   key_vault_id = azurerm_key_vault.api_secrets.id
+
+  depends_on = [time_sleep.wait_for_key_vault_policy]
 }
--- main.tf
+++ main.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "4.55.0"
     }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "0.12.1"
+    }
   }
 }
 
