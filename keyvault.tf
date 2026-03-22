09dc2790d495932f4de4f3e607b80200237de4c0
Chakravarthy Peela <chakripeela@gmail.com>
2026-03-22 12:42:46 +0530

    added rbac

--- keyvault.tf
+++ keyvault.tf
@@ -6,6 +6,7 @@ resource "azurerm_key_vault" "api_secrets" {
   resource_group_name = module.resource_group.resource_group_name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
+  enable_rbac_authorization = true
 
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
@@ -15,25 +16,16 @@ resource "azurerm_key_vault" "api_secrets" {
   }
 }
 
-resource "azurerm_key_vault_access_policy" "current" {
-  key_vault_id = azurerm_key_vault.api_secrets.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-
-  secret_permissions = [
-    "Get",
-    "List",
-    "Set",
-    "Delete",
-    "Recover",
-    "Purge"
-  ]
+resource "azurerm_role_assignment" "current_key_vault_secrets_officer" {
+  scope                = azurerm_key_vault.api_secrets.id
+  role_definition_name = "Key Vault Secrets Officer"
+  principal_id         = data.azurerm_client_config.current.object_id
 }
 
 resource "time_sleep" "wait_for_key_vault_policy" {
-  depends_on = [azurerm_key_vault_access_policy.current]
+  depends_on = [azurerm_role_assignment.current_key_vault_secrets_officer]
 
-  create_duration = "30s"
+  create_duration = "60s"
 }
 
 resource "azurerm_key_vault_secret" "db_server" {
