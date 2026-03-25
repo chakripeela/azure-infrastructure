0d10d7efad8b9b1fa8580492fbc9478d72de763c
Chakravarthy Peela <chakripeela@gmail.com>
2026-03-25 19:53:26 +0530

    fix: add inbound security rule for App Gateway V2

--- virtual-network/main.tf
+++ virtual-network/main.tf
@@ -73,6 +73,19 @@ resource "azurerm_network_security_group" "appgw_nsg" {
   location            = var.location
   resource_group_name = var.resource_group_name
 }
+resource "azurerm_network_security_rule" "appgw_allow_appgw_v2_inbound" {
+  name                        = "AllowAppGwV2Inbound"
+  priority                    = 105
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "65200-65535"
+  source_address_prefix       = "Internet"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.appgw_nsg.name
+}
 
 resource "azurerm_network_security_rule" "appgw_allow_gateway_manager" {
   name                        = "AllowGatewayManagerInbound"
