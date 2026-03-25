cb74428c3925667f7a063c0d0251662dc437ef62
Chakravarthy Peela <chakripeela@gmail.com>
2026-03-26 01:46:10 +0530

    feat: implement Azure CDN Front Door resources and routing configuration

--- compute/frontdoor/main.tf
+++ compute/frontdoor/main.tf
@@ -1,69 +1,67 @@
-# Azure Front Door Terraform resource (minimal example)
-resource "azurerm_frontdoor" "main" {
-  count               = var.gateway_type == "frontdoor" ? 1 : 0
+
+# Azure Front Door Standard/Premium (CDN) resources
+resource "azurerm_cdn_frontdoor_profile" "main" {
   name                = "fd-${var.application_name}-${var.location}"
   resource_group_name = var.resource_group_name
-  # VNet integration (subnet_id) for Front Door Premium (if supported)
-  # Uncomment the following line if using Premium SKU and VNet integration is required:
-  # subnet_id = var.subnet_id
+  sku_name            = "Standard_AzureFrontDoor"
+}
 
-  frontend_endpoint {
-    name                              = "default-frontend-endpoint"
-    host_name                         = "fd-${var.application_name}-${var.location}.azurefd.net"
+resource "azurerm_cdn_frontdoor_endpoint" "main" {
+  name                     = "fd-endpoint-${var.application_name}-${var.location}"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.main.id
+}
+
+resource "azurerm_cdn_frontdoor_origin_group" "main" {
+  name                     = "fd-origingroup-${var.application_name}-${var.location}"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.main.id
+
+  load_balancing {
+    sample_size                        = 4
+    successful_samples_required        = 3
+    additional_latency_in_milliseconds = 50
   }
 
-  # App Service (UI)
-  backend_pool {
-    name = "backend-pool-appservice"
-    backend {
-      host_header = var.app_service_fqdn
-      address     = var.app_service_fqdn
-      http_port   = 80
-      https_port  = 443
-      weight      = 50
-      priority    = 1
-    }
-    load_balancing_name = "default-lb"
-    health_probe_name   = "default-probe"
-  }
-
-  # AKS API (internal LB)
-  backend_pool {
-    name = "backend-pool-aks-api"
-    backend {
-      host_header = var.backend_ip
-      address     = var.backend_ip
-      http_port   = 80
-      https_port  = 443
-      weight      = 50
-      priority    = 1
-    }
-    load_balancing_name = "default-lb"
-    health_probe_name   = "default-probe"
-  }
-
-  backend_pool_health_probe {
-    name                = "default-probe"
-    protocol            = "Http"
-    path                = "/"
+  health_probe {
     interval_in_seconds = 30
+    path                = "/"
+    protocol            = "Http"
   }
+}
 
-  backend_pool_load_balancing {
-    name                            = "default-lb"
-    sample_size                     = 4
-    successful_samples_required     = 2
-    additional_latency_milliseconds = 0
-  }
+resource "azurerm_cdn_frontdoor_origin" "appservice" {
+  name                            = "appservice-origin"
+  cdn_frontdoor_origin_group_id   = azurerm_cdn_frontdoor_origin_group.main.id
+  host_name                       = var.app_service_fqdn
+  http_port                       = 80
+  https_port                      = 443
+  enabled                         = true
+  certificate_name_check_enabled   = false
+  priority                        = 1
+  weight                          = 1000
+}
 
-  routing_rule {
-    name               = "default-routing-rule"
-    accepted_protocols = ["Http", "Https"]
-    patterns_to_match  = ["/*"]
-    frontend_endpoints = ["default-frontend-endpoint"]
-    forwarding_configuration {
-      forwarding_protocol = "MatchRequest"
-      backend_pool_name   = "default-backend-pool"
-    }
-  }
+resource "azurerm_cdn_frontdoor_origin" "aksapi" {
+  name                            = "aksapi-origin"
+  cdn_frontdoor_origin_group_id   = azurerm_cdn_frontdoor_origin_group.main.id
+  host_name                       = var.backend_ip
+  http_port                       = 80
+  https_port                      = 443
+  enabled                         = true
+  certificate_name_check_enabled   = false
+  priority                        = 2
+  weight                          = 1000
+}
+
+resource "azurerm_cdn_frontdoor_route" "main" {
+  name                           = "fd-route-${var.application_name}-${var.location}"
+  cdn_frontdoor_endpoint_id      = azurerm_cdn_frontdoor_endpoint.main.id
+  cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.main.id
+  cdn_frontdoor_origin_ids       = [
+    azurerm_cdn_frontdoor_origin.appservice.id,
+    azurerm_cdn_frontdoor_origin.aksapi.id
+  ]
+  patterns_to_match              = ["/*"]
+  supported_protocols            = ["Http", "Https"]
+  forwarding_protocol            = "MatchRequest"
+  https_redirect_enabled         = true
 }
