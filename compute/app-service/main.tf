a80419c2640276e3e6a6e5d1c1d840043e1e1f74
Chakravarthy Peela <chakripeela@gmail.com>
2026-03-31 09:12:48 +0530

    update sku

--- compute/app-service/main.tf
+++ compute/app-service/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "plan" {
   location            = var.location
   resource_group_name = var.resource_group_name
   os_type             = "Linux"
-    sku_name            = "P1v2"
+    sku_name            = "S1"
 }
 
 resource "azurerm_linux_web_app" "app" {
