3b6fa9f3dcd25f964bf4003f17872fa6e37e4f9a
Chakravarthy Peela <chakripeela@gmail.com>
2026-03-31 12:48:03 +0530

    fix syntaxes

--- main.tf
+++ main.tf
@@ -108,9 +108,9 @@ module "app_gateway_dr" {
   source              = "./compute/app-gateway"
   application_name    = "${var.application_name}-dr"
   location            = var.dr_location
-  resource_group_name = module.resource_group_dr.resource_group_name
-  subnet_id           = module.virtual_network_dr.subnet_appgw_id
-  app_service_fqdn    = module.app_service_dr.app_service_default_hostname
+  resource_group_name = module.resource_group_dr[0].resource_group_name
+  subnet_id           = module.virtual_network_dr[0].subnet_appgw_id
+  app_service_fqdn    = module.app_service_dr[0].app_service_default_hostname
   backend_ip          = "10.1.2.250"
   appgw_nsg_assoc_id = module.virtual_network.appgw_nsg_assoc_id
 }
@@ -121,9 +121,9 @@ module "sql_dr" {
   source                  = "./data/sql"
   application_name        = "${var.application_name}-dr"
   location                = var.dr_location
-  resource_group_name     = module.resource_group_dr.resource_group_name
-  subnet_id               = module.virtual_network_dr.subnet_sql_id
-  sql_private_dns_zone_id = module.virtual_network_dr.sql_private_dns_zone_id
+  resource_group_name     = module.resource_group_dr[0].resource_group_name
+  subnet_id               = module.virtual_network_dr[0].subnet_sql_id
+  sql_private_dns_zone_id = module.virtual_network_dr[0].sql_private_dns_zone_id
   sql_server_name         = "${var.sql_server_name}-dr"
   sql_database_name       = "${var.sql_database_name}-dr"
   sql_aad_admin_login     = var.sql_aad_admin_login
@@ -135,8 +135,8 @@ module "aks_dr" {
   count = var.is_dr ? 1 : 0
   source                = "./compute/aks"
   location              = var.dr_location
-  resource_group_name   = module.resource_group_dr.resource_group_name
-  subnet_id             = module.virtual_network_dr.subnet_aks_id
+  resource_group_name   = module.resource_group_dr[0].resource_group_name
+  subnet_id             = module.virtual_network_dr[0].subnet_aks_id
 }
 
 # DR region ACR
@@ -146,9 +146,9 @@ module "acr_dr" {
   application_name      = "${var.application_name}-dr"
   acr_name              = "chakripeelaacrdr"
   location              = var.dr_location
-  resource_group_name   = module.resource_group_dr.resource_group_name
-  subnet_id             = module.virtual_network_dr.subnet_acr_private_endpoint_id
-  acr_private_dns_zone_id = module.virtual_network_dr.acr_private_dns_zone_id
+  resource_group_name   = module.resource_group_dr[0].resource_group_name
+  subnet_id             = module.virtual_network_dr[0].subnet_acr_private_endpoint_id
+  acr_private_dns_zone_id = module.virtual_network_dr[0].acr_private_dns_zone_id
 }
 
 # DR region app service
@@ -157,8 +157,8 @@ module "app_service_dr" {
   source                = "./compute/app-service"
   plan_name             = "${var.application_name}-dr-plan"
   location              = var.dr_location
-  resource_group_name   = module.resource_group_dr.resource_group_name
-  subnet_id             = module.virtual_network_dr.subnet_appsvc_id
+  resource_group_name   = module.resource_group_dr[0].resource_group_name
+  subnet_id             = module.virtual_network_dr[0].subnet_appsvc_id
   enabled               = false
 }
 
@@ -168,6 +168,6 @@ module "virtual_network_dr" {
   source                = "./virtual-network"
   application_name      = "${var.application_name}-dr"
   location              = var.dr_location
-  resource_group_name   = module.resource_group_dr.resource_group_name
-  shared_resource_group = module.resource_group_dr.shared_resource_group_name
+  resource_group_name   = module.resource_group_dr[0].resource_group_name
+  shared_resource_group = module.resource_group_dr[0].shared_resource_group_name
 }
