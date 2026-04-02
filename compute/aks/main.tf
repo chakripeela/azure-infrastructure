f85233b0d1c246b968b8fa562efd9a96cc3b28e9
Chakravarthy Peela <chakripeela@gmail.com>
2026-04-02 20:46:45 +0530

    udpate vm size

--- compute/aks/main.tf
+++ compute/aks/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_B16ps_v2"
+    vm_size    = "Standard_DC2s_v3"
     os_disk_size_gb = 30
     vnet_subnet_id = var.subnet_id
   }
