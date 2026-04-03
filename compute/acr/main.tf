3141243565ec6df414ef344ceeb6301f7f385241
Chakravarthy Peela <chakripeela@gmail.com>
2026-04-04 01:52:43 +0530

    enabline PE for acr

--- compute/acr/main.tf
+++ compute/acr/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Premium"
-  public_network_access_enabled = true
+  public_network_access_enabled = false
 
   identity {
     type = "SystemAssigned"
